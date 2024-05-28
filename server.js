9a59aff8c77a83bd081131edde5787b291fc9a4c
L-Lainio <arollainio@gmail.com>
2024-05-28 14:51:59 -0400

    restructure folders

--- server.js
+++ server.js
@@ -23,9 +23,6 @@ app.get('/api/notes', (req, res) => {
 		res.json(dbData)
 	});
 })
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, './public/index.html'));
-});
 
 app.get('/notes', (req, res) => {
 	res.sendFile(path.join(__dirname, './public/notes.html'));
